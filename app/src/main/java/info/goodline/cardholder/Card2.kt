66f1c7f539a1accf8c60d15d4fbd3669a67d18aa
Izaky <Izakynash@gmail.com>
2019-03-26 02:29:43 +0700

    to Kotlin 4

--- app/src/main/java/info/goodline/cardholder/Card2.kt
+++ app/src/main/java/info/goodline/cardholder/Card2.kt
@@ -2,25 +2,32 @@ package info.goodline.cardholder
 
 import android.os.Parcel
 import android.os.Parcelable
-import kotlinx.android.parcel.Parceler
-import kotlinx.android.parcel.Parcelize
+//import kotlinx.android.parcel.Parceler
+//import kotlinx.android.parcel.Parcelize
 
-@Parcelize
+//@Parcelize
 class Card2 (val title: String?, val id: Int, val photo: String?) : Parcelable {
 
     constructor (`in`: Parcel): this (`in`.readString(),`in`.readInt(),`in`.readString() )
 
-    companion object : Parceler<Card2> {
-
-        override fun Card2.write(parcel: Parcel, flags: Int) {
-            parcel.writeString(title)
-            parcel.writeString(photo)
-            parcel.writeInt(id)
-        }
-
-        override fun create(parcel: Parcel): Card2 {
-            return Card2(parcel)
-        }
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeInt(id)
+        parcel.writeString(photo)
     }
 
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Card2> {
+        override fun createFromParcel(parcel: Parcel): Card2 {
+            return Card2(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Card2?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
+
